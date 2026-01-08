7c3bc568b67b87ad9a55c1635b978247d648dbc2
papalexios001-wq <papalexios001@gmail.com>
2026-01-08 17:39:09 +0200

    Update index.ts

--- src/components/strategy/index.ts
+++ src/components/strategy/index.ts
@@ -1,1 +1,15 @@
+// src/components/strategy/index.ts
+// Barrel export for strategy components
+
+export { ActivityLog } from './ActivityLog';
+export { BulkMode } from './BulkMode';
+export { ContentBriefGenerator } from './ContentBriefGenerator';
+export { ContentStrategyDashboard } from './ContentStrategyDashboard';
+export { DashboardMetrics } from './DashboardMetrics';
+export { OptimizationProgress } from './OptimizationProgress';
+export { PageQueue } from './PageQueue';
+export { QuickOptimize } from './QuickOptimize';
+export { SerpAnalyzer } from './SerpAnalyzer';
+export { SerpIntelligence } from './SerpIntelligence';
+export { SitemapCrawler } from './SitemapCrawler';
 
