9e46e2be1f9a6e3bd29d6cac3b4542a3fb57da61
papalexios001-wq <papalexios001@gmail.com>
2026-01-08 18:19:58 +0200

    Update index.ts

--- src/components/strategy/index.ts
+++ src/components/strategy/index.ts
@@ -1,15 +1,16 @@
 // src/components/strategy/index.ts
-// Barrel export for strategy components
+// Only export components that ACTUALLY exist
 
 export { ActivityLog } from './ActivityLog';
 export { BulkMode } from './BulkMode';
-export { ContentBriefGenerator } from './ContentBriefGenerator';
-export { ContentStrategyDashboard } from './ContentStrategyDashboard';
 export { DashboardMetrics } from './DashboardMetrics';
 export { OptimizationProgress } from './OptimizationProgress';
 export { PageQueue } from './PageQueue';
 export { QuickOptimize } from './QuickOptimize';
-export { SerpAnalyzer } from './SerpAnalyzer';
 export { SerpIntelligence } from './SerpIntelligence';
 export { SitemapCrawler } from './SitemapCrawler';
 
+// NOTE: Uncomment these when you create the files:
+// export { ContentBriefGenerator } from './ContentBriefGenerator';
+// export { SerpAnalyzer } from './SerpAnalyzer';
+// export { ContentStrategyDashboard } from './ContentStrategyDashboard';
