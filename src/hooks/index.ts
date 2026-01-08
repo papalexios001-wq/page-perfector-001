219c0ae5e7183e3888958aaa3a938467df867495
papalexios001-wq <papalexios001@gmail.com>
2026-01-08 17:39:45 +0200

    Update index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,9 @@
+// src/hooks/index.ts
+// Barrel export for hooks
+
+export { useContentBrief } from './use-content-brief';
+export { useContentOptimization } from './use-content-optimization';
+export { useSerpAnalysis } from './use-serp-analysis';
+export { useMobile } from './use-mobile';
+export { useToast } from './use-toast';
 
